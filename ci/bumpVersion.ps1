60718092a7241d4d0c70646d4aca360e36dcda1a
Jason Watson <jbw@jbw.cc>
2022-01-08 17:58:21 +0000

    chore: move into IDE specific directory

--- ci/bumpVersion.ps1
+++ ci/bumpVersion.ps1
@@ -2,7 +2,7 @@ $version = $args[0]
 Write-Host "Set version: $version"
 
 # TODO: Replace the path with the path to your vsixmanifest file
-$FullPath = Resolve-Path $PSScriptRoot\..\source.extension.vsixmanifest
+$FullPath = Resolve-Path $PSScriptRoot\..\vs\source.extension.vsixmanifest
 Write-Host $FullPath
 [xml]$content = Get-Content $FullPath
 $content.PackageManifest.Metadata.Identity.Version = $version
